3e2a5ffcb5da1e64086c4a9d1928b66732dfa4ac
Alex Stoyanov <Asstoyanov20@codingburgas.bg>
2024-12-11 21:45:48 +0200

    Add project

---
+++ HrManagement/HrManagement.DataAccess/Data/HrManagementDbContext.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using HrManagement.DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HrManagement.DataAccess.Data;
+
+public partial class HrManagementDbContext : DbContext
+{
+    public HrManagementDbContext()
+    {
+    }
+
+    public HrManagementDbContext(DbContextOptions<HrManagementDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Assignment> Assignments { get; set; }
+
+    public virtual DbSet<Department> Departments { get; set; }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    public virtual DbSet<Project> Projects { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=HrManagement;Trusted_Connection=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Assignment>(entity =>
+        {
+            entity.Property(e => e.Id).ValueGeneratedNever();
+
+            entity.HasOne(d => d.Employee).WithMany(p => p.Assignments)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Assignments_Employees");
+
+            entity.HasOne(d => d.Project).WithMany(p => p.Assignments)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Assignments_Projects");
+        });
+
+        modelBuilder.Entity<Department>(entity =>
+        {
+            entity.Property(e => e.Id).ValueGeneratedNever();
+        });
+
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.Property(e => e.Id).ValueGeneratedNever();
+
+            entity.HasOne(d => d.Department).WithMany(p => p.Employees)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Employees_Departments");
+        });
+
+        modelBuilder.Entity<Project>(entity =>
+        {
+            entity.Property(e => e.Id).ValueGeneratedNever();
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ HrManagement/HrManagement.DataAccess/Repository/Base/BaseRepository.cs
@@ -0,0 +1,58 @@
+﻿using HrManagement.DataAccess.Data;
+using HrManagement.DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace HrManagement.DataAccess.Repository.Base
+{
+    public abstract class BaseRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly HrManagementDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        protected BaseRepository(HrManagementDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public virtual T? GetById(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public virtual IQueryable<T> GetAll()
+        {
+            return _dbSet.AsQueryable();
+        }
+
+        public virtual bool Add(T entity)
+        {
+            _dbSet.Add(entity);
+            return _context.SaveChanges() > 0;
+        }
+
+        public virtual bool Update(T entity)
+        {
+            var trackedEntity = _dbSet.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+            return _context.SaveChanges() > 0;
+        }
+
+        protected abstract void UpdateEntity(T existingEntity, T newEntity);
+
+        public virtual bool Delete(int id)
+        {
+            var entity = GetById(id);
+            if (entity == null) return false;
+
+            _dbSet.Remove(entity);
+            DeleteAdditionalDependencies(entity);
+            return _context.SaveChanges() > 0;
+        }
+
+        protected virtual void DeleteAdditionalDependencies(T entity)
+        {
+        }
+    }
+}
